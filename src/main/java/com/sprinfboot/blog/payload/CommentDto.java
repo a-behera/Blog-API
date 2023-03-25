4caccf0762d3f8ad03be91414e3e6447a4a81e45
a-behera <ankitbehera799@gmail.com>
2023-03-25 21:02:29 +0530

    Spring Boot REST API Validation

--- src/main/java/com/sprinfboot/blog/payload/CommentDto.java
+++ src/main/java/com/sprinfboot/blog/payload/CommentDto.java
@@ -2,10 +2,19 @@ package com.sprinfboot.blog.payload;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 public class CommentDto {
-   private long id;
+    private long id;
+    @NotEmpty(message = "Name Should not be null or empty")
     private String name;
+    @NotEmpty(message = "Email is required")
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 10,message = "min 10 character required")
     private String body;
 }
